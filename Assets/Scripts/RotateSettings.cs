1d83ce5debf99559ceae559a7ca892af322718fa
sanne14 <matthewkim1034@gmail.com>
2024-07-31 13:48:51 +0900

    Setting Icon ADD

---
+++ Assets/Scripts/RotateSettings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSettings : MonoBehaviour
+{
+    public float rotationSpeed = 100f;  // 아이콘이 회전하는 속도
+
+    void Update()
+    {
+        // 매 프레임마다 아이콘을 회전시킵니다.
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
