4295ed11bffd3506580756c8987a4017233deea6
leeSEOyoung <seosuper01@snu.ac.kr>
2024-08-10 19:57:10 +0900

    EndingCompilation modified

--- Assets/Scripts/EndingCompilation.cs
+++ Assets/Scripts/EndingCompilation.cs
@@ -1,50 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class EndingCompilation : MonoBehaviour
 {
-	public void BackToMainMenu()
+	private UIDocument _doc;
+	private Button _backButton;
+	private Button _ending1;
+	private Button _ending2;
+	private Button _ending3;
+	private Button _ending4;
+	private Button _ending5;
+
+	private void Awake()
+	{
+		_doc = GetComponent<UIDocument>();
+
+		_backButton = _doc.rootVisualElement.Q<Button>("BackButton");
+		_ending1		= _doc.rootVisualElement.Q<Button>("EndingButton1");
+		_ending2		= _doc.rootVisualElement.Q<Button>("EndingButton2");
+		_ending3		= _doc.rootVisualElement.Q<Button>("EndingButton3");
+		_ending4		= _doc.rootVisualElement.Q<Button>("EndingButton4");
+		_ending5		= _doc.rootVisualElement.Q<Button>("EndingButton5");
+
+		_backButton.clicked += BackButtonClicked;
+		_ending1.clicked		+= Ending1Clicked;
+		_ending2.clicked		+= Ending2Clicked;
+		_ending3.clicked		+= Ending3Clicked;
+		_ending4.clicked		+= Ending4Clicked;
+		_ending5.clicked		+= Ending5Clicked;
+	}
+	private void BackButtonClicked()
 	{
 		SceneManager.LoadScene("MainMenu");
 	}
-
-	public void Ending1()
+	private void Ending1Clicked()
 	{
 		Debug.Log("Ending1");
 	}
-	public void Ending2()
+	private void Ending2Clicked()
 	{
 		Debug.Log("Ending2");
 	}
-	public void Ending3()
+	private void Ending3Clicked()
 	{
 		Debug.Log("Ending3");
 	}
-	public void Ending4()
+	private void Ending4Clicked()
 	{
 		Debug.Log("Ending4");
 	}
-	public void Ending5()
+	private void Ending5Clicked()
 	{
 		Debug.Log("Ending5");
-	}
-	public void Ending6()
-	{
-		Debug.Log("Ending6");
-	}
-	public void Ending7()
-	{
-		Debug.Log("Ending7");
-	}
-	public void Ending8()
-	{
-		Debug.Log("Ending8");
-	}
-	public void Ending9()
-	{
-		Debug.Log("Ending9");
 	}
 }
