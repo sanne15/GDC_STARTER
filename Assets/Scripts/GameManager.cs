620be893193058495d5b62521cbbd29b7e7a2bf1
sanne14 <matthewkim1034@gmail.com>
2024-08-22 19:45:56 +0900

    GameManagerIndentationEdited
    GameManagerIndentationEdited

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,58 +6,75 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-  private UIDocument _doc;
-  private Button _startButton;
-  private Button _quitButton;
-  private Button _settingsButton;
-  private Button _endingButton;
-  private Button _loadButton;
+    private UIDocument _doc;
+    private Button _startButton;
+    private Button _quitButton;
+    private Button _settingsButton;
+    private Button _endingButton;
+    private Button _loadButton;
 
-  private FadeInScript _fadeInScript;
+    private FadeInScript _fadeInScript;
 
-	private void Awake()
-	{
-		_doc = GetComponent<UIDocument>();
+    private void Awake()
+    {	
+        _doc = GetComponent<UIDocument>();
+        var root = _doc.rootVisualElement;
+    
+        if (root == null)
+        {
+            Debug.LogError("rootVisualElement is null!");
+            return;
+        }
 
-    _startButton    = _doc.rootVisualElement.Q<Button>("StartButton");
-    _quitButton     = _doc.rootVisualElement.Q<Button>("QuitButton");
-    _settingsButton = _doc.rootVisualElement.Q<Button>("SettingsButton");
-    _endingButton   = _doc.rootVisualElement.Q<Button>("EndingButton");
-    _loadButton     = _doc.rootVisualElement.Q<Button>("LoadButton");
+        _startButton = root.Q<Button>("StartButton");
+        _quitButton = root.Q<Button>("QuitButton");
+        _settingsButton = root.Q<Button>("SettingsButton");
+        _endingButton = root.Q<Button>("EndingButton");
+        _loadButton = root.Q<Button>("LoadButton");
 
-    _startButton.clicked    += StartButtonClicked;
-    _quitButton.clicked     += QuitButtonClicked;
-    _settingsButton.clicked += SettingsButtonClicked;
-    _endingButton.clicked   += EndingButtonClicked;
-    _loadButton.clicked     += LoadButtonClicked;
+            if (_startButton == null)
+        {
+            Debug.LogError("StartButton not found in the UI document.");
+        }
 
-    _fadeInScript = GameObject.Find("GoToOpeningScene Panel").GetComponent<FadeInScript>();
-	}
+        _startButton.clicked    += StartButtonClicked;
+        _quitButton.clicked     += QuitButtonClicked;
+        _settingsButton.clicked += SettingsButtonClicked;
+        _endingButton.clicked   += EndingButtonClicked;
+        _loadButton.clicked     += LoadButtonClicked;
 
-  private void StartButtonClicked()
-  {
-    _fadeInScript.StartFadeIn();
-    Invoke("GoToOpeningScene", _fadeInScript.fadeTime);
-  }
+        _fadeInScript = GameObject.Find("GoToOpeningScene Panel").GetComponent<FadeInScript>();
 
-  private void GoToOpeningScene()
-  {
-		SceneManager.LoadScene("Opening");
-	}
-  private void QuitButtonClicked()
-  {
-    Application.Quit();
-  }
-  private void SettingsButtonClicked()
-  {
-		SceneManager.LoadScene("Settings");
-	}
-  private void EndingButtonClicked()
-  {
-		SceneManager.LoadScene("EndingCompilation");
-	}
-  private void LoadButtonClicked()
-  {
-    Debug.Log("저장 미구현...");
-  }
+    }
+
+    private void StartButtonClicked()
+    {
+        _fadeInScript.StartFadeIn();
+        Invoke("GoToOpeningScene", _fadeInScript.fadeTime);
+    }
+
+    private void GoToOpeningScene()
+    {
+	    SceneManager.LoadScene("Opening");
+    }
+
+    private void QuitButtonClicked()
+    {
+        Application.Quit();
+    }
+
+    private void SettingsButtonClicked()
+    {
+	    SceneManager.LoadScene("Settings");
+    }
+
+    private void EndingButtonClicked()
+    {
+	    SceneManager.LoadScene("EndingCompilation");
+    }
+
+    private void LoadButtonClicked()
+    {
+        Debug.Log("저장 미구현...");
+    }
 }
